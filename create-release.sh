55d671dcfa127909b6a91c5e0a7adb8bd3fcac9b
Chenxiong Qi <qcxhome@gmail.com>
2026-02-11 22:22:47 +0800

    update script

--- create-release.sh
+++ create-release.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-snapshot=$1
+snapshot=${1:?Missing snapshot name}
 yq ".spec.snapshot |= \"${snapshot}\"" releases/init-release.yaml >/tmp/init-release.yaml
 printf "Apply Release:\n\n"
 trap 'rm /tmp/init-release.yaml' EXIT ERR
 cat /tmp/init-release.yaml
+printf "\n"
 kubectl create -f /tmp/init-release.yaml -n cqi-tenant

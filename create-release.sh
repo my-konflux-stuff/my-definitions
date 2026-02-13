b4f0e32947b0b2513f2a4f0b8553c4f0f236d896
Chenxiong Qi <qcxhome@gmail.com>
2026-02-13 20:04:59 +0800

    Restrict working directory

--- create-release.sh
+++ create-release.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 snapshot=${1:?Missing snapshot name}
 temp_release_cr=/tmp/release-cr.yaml
+if ! [[ -f releases/release-templ.yaml ]]
+then
+    printf "The current working directory seems not correct. Cannot find releases/release-templ.yaml\n" >&2
+    exit 1
+fi
 yq ".spec.snapshot |= \"${snapshot}\"" releases/release-templ.yaml >"$temp_release_cr"
 printf "Apply Release:\n\n"
 trap 'rm "$temp_release_cr"' EXIT ERR
